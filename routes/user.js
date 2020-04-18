f6fd44b317942b8006817f72b35c50ef760f69e0
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-18 02:55:27 -0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ app.get("/", (req, res, next) => {
         });
       }
 
-      User.count({}, (err, count) => {
+      User.countDocuments({}, (err, count) => {
         res.status(200).json({
           ok: true,
           total: count,
